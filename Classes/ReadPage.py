5231f000981d4fd0aebcb1b5b94c2ede0b5e065a
Elihu Cruz Albores <ea_2305@hotmail.com>
2016-04-15 14:52:17 -0500

    Start funcitons

---
+++ Classes/ReadPage.py
@@ -0,0 +1,8 @@
+class ReadPage(object):
+
+    """docstring for ReadPage """
+    def __init__(self, file = ""):
+        self.CurrentFile = file
+
+    def openPage(self):
+        print "Hola"
---
+++ Main.py
@@ -0,0 +1,9 @@
+""" Call classes in main class"""
+from Classes.ReadPage import ReadPage
+
+print "No la haga compa"
+
+
+x =  ReadPage("hola")
+
+print x.openPage()
